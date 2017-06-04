8409ee632d691c94cb9f7455916eb551d0f6b3b8
Ira <smalko.irina.s@gmail.com>
2017-06-04 18:13:00 +0300

    added zooming in ConstructorActivity

--- app/src/main/java/not/dresser/activities/ConstructorActivity.java
+++ app/src/main/java/not/dresser/activities/ConstructorActivity.java
@@ -2,60 +2,56 @@ package not.dresser.activities;
 
 
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 import not.dresser.R;
+import not.dresser.helpers.ZoomInZoomOutHelper;
 
-import static not.dresser.activities.ShelfListActivity.CATEGORY;
 import static not.dresser.activities.ShelfListActivity.PHOTO_URL;
 
-public class ConstructorActivity extends AppCompatActivity {
+public class ConstructorActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private Camera camera;
+    private static final String TAG = "Touch";
+    @SuppressWarnings("unused")
+    private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
+
+    // These matrices will be used to scale points of the image
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+
+    // The 3 states (events) which the user is trying to perform
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    // these PointF objects are used to record the point(s) the user is touching
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constructor_activity);
 
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int screenWidth = displaymetrics.widthPixels;
-        int screenHeight = displaymetrics.heightPixels;
-        int imageViewSize = screenWidth * 3 / 4;
-
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface_view);
 
         Intent intent = getIntent();
-
-        switch (intent.getStringExtra(CATEGORY)) {
-            case "pants":
-                imageView.setY(screenHeight * 1 / 3);
-                break;
-            case "skirt":
-            case "shorts":
-                imageView.setY(screenHeight * 1/3);
-                imageView.getLayoutParams().width = screenWidth * 2 / 4;
-                imageView.getLayoutParams().height = screenWidth * 2 / 4;
-                break;
-            case "dress":
-                imageView.getLayoutParams().width = screenWidth;
-                imageView.getLayoutParams().height = screenWidth;
-                break;
-            default:
-                imageView.getLayoutParams().width = imageViewSize;
-                imageView.getLayoutParams().height = imageViewSize;
-        }
-
         Glide.with(this).load(intent.getStringExtra(PHOTO_URL)).into(imageView);
 
         SurfaceHolder holder = surfaceView.getHolder();
@@ -80,6 +76,8 @@ public class ConstructorActivity extends AppCompatActivity {
             public void surfaceDestroyed(SurfaceHolder holder) {
             }
         });
+
+        imageView.setOnTouchListener(this);
     }
 
     @Override
@@ -95,4 +93,70 @@ public class ConstructorActivity extends AppCompatActivity {
             camera.release();
         camera = null;
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        ImageView view = (ImageView) v;
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+        ZoomInZoomOutHelper zoomHelper = new ZoomInZoomOutHelper();
+        zoomHelper.dumpEvent(event);
+        // Handle touch events here...
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:   // first finger down only
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                Log.d(TAG, "mode=DRAG"); // write to LogCat
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_UP: // first finger lifted
+
+            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+
+                mode = NONE;
+                Log.d(TAG, "mode=NONE");
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+
+                oldDist = zoomHelper.spacing(event);
+                Log.d(TAG, "oldDist=" + oldDist);
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    zoomHelper.midPoint(mid, event);
+                    mode = ZOOM;
+                    Log.d(TAG, "mode=ZOOM");
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+                } else if (mode == ZOOM) {
+                    // pinch zooming
+                    float newDist = zoomHelper.spacing(event);
+                    Log.d(TAG, "newDist=" + newDist);
+                    if (newDist > 5f) {
+                        matrix.set(savedMatrix);
+                        scale = newDist / oldDist; // setting the scaling of the
+                        // matrix...if scale > 1 means
+                        // zoom in...if scale < 1 means
+                        // zoom out
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+
+        view.setImageMatrix(matrix); // display the transformation on screen
+
+        return true; // indicate event was handled
+    }
+
+
 }
---
+++ app/src/main/java/not/dresser/helpers/ZoomInZoomOutHelper.java
@@ -0,0 +1,56 @@
+package not.dresser.helpers;
+
+import android.graphics.PointF;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class ZoomInZoomOutHelper {
+
+    public float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
+     * Description: calculates the midpoint between the two fingers
+     * ------------------------------------------------------------
+     */
+
+    public void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    /**
+     * Show an event in the LogCat view, for debugging
+     */
+    public void dumpEvent(MotionEvent event) {
+        String names[] = {"DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE", "POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?"};
+        StringBuilder sb = new StringBuilder();
+        int action = event.getAction();
+        int actionCode = action & MotionEvent.ACTION_MASK;
+        sb.append("event ACTION_").append(names[actionCode]);
+
+        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) {
+            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+            sb.append(")");
+        }
+
+        sb.append("[");
+        for (int i = 0; i < event.getPointerCount(); i++) {
+            sb.append("#").append(i);
+            sb.append("(pid ").append(event.getPointerId(i));
+            sb.append(")=").append((int) event.getX(i));
+            sb.append(",").append((int) event.getY(i));
+            if (i + 1 < event.getPointerCount())
+                sb.append(";");
+        }
+
+        sb.append("]");
+        Log.d("Touch Events ---------", sb.toString());
+    }
+}
